bd031deb40b89f7ff644305244060555bf17925b
bama16 <64561829+bama16@users.noreply.github.com>
2025-08-28 19:42:44 +0700

    modifikasi databaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,10 +14,33 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+        $user = [
+            [
+                'name' => 'Admin',
+                'email' => 'oprator@gmail.com',
+                'password' => bcrypt('admin123'),
+                'role' => 'oprator',
+            ],
+            [
+                'name' => 'Admin',
+                'email' => 'admin@gmail.com',
+                'password' => bcrypt('admin123'),
+                'role' => 'admin',
+            ],
+            [
+                'name' => 'Pimpinan',
+                'email' => 'pimpinan@gmail.com',
+                'password' => bcrypt('admin123'),
+                'role' => 'pimpinan',
+            ],
+        ];
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+        foreach ($user as $key => $value) {
+            User::create($value);
+        }
     }
 }
