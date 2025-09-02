0619be22aba1ef6fcd5fb9a0f268e67dbe1f1f82
bama16 <64561829+bama16@users.noreply.github.com>
2025-09-02 18:49:37 +0700

    menambahkan halaman dashboard

---
+++ resources/views/pages/dashboard.blade.php
@@ -0,0 +1,124 @@
+@extends('layouts.dashboard-admin')
+
+@section('content')
+<main class="context px-3 py-2">
+    <div class="container-fluid">
+        <div class="mb-3">
+
+            <!--Section Card-->
+            <div class="container-fluid">
+                <div class="row row-cols-lg-3 mt-3">
+
+                    <a href="#">
+                        <div class="col-12 d-flex">
+                            <div class="card flex-fill border-0 kotak">
+                                <div class="card-body py 4">
+                                    <div class="d-flex align-item-start">
+                                        <div class="flex-grown-1">
+                                            <h5 class="mb2">Jumlah Barang Masuk</h5>
+                                            <h4 class="mb-2">{{ $jumlahMasuk }}</h4>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                    <a href="#">
+                        <div class="col-12 d-flex">
+                            <div class="card flex-fill border-0 kotak">
+                                <div class="card-body py 4">
+                                    <div class="d-flex align-item-start">
+                                        <div class="flex-grown-1">
+                                            <h5 class="mb2">Jumlah Barang Keluar</h5>
+                                            <h4 class="mb-2">{{ $jumlahKeluar }}</h4>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                    <a href="#">
+                        <div class="col-12 d-flex">
+                            <div class="card flex-fill border-0 kotak">
+                                <div class="card-body py 4">
+                                    <div class="d-flex align-item-start">
+                                        <div class="flex-grown-1">
+                                            <h5 class="mb2">Jumlah Supplier</h5>
+                                            <h4 class="mb-2">{{ $jumlahSupplier }}</h4>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </a>                    
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row row-cols-lg-3 mt-4">
+        <div class="container">
+            <canvas id="barChart" style="width: 100%; height: 400px;"></canvas>
+        </div>
+        <div class="container">
+            <canvas id="pieChart" style="width: 100%; height: 200px;"></canvas>
+        </div>
+    </div>
+</main>
+
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<script>
+    var ctx = document.getElementById('barChart').getContext('2d');
+    var categories = @json($itemCountsCategory->pluck('kategori'));
+    var itemCounts = @json($itemCountsCategory->pluck('count'));
+
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: categories,
+            datasets: [{
+                label: 'Jumlah Barang per Kategori',
+                data: itemCounts,
+                backgroundColor: 'rgba(54, 162, 235, 0.5)',
+                borderColor: 'rgba(54, 162, 235, 1)',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true,
+                    // precision: 0
+                }
+            }
+        }
+    });
+</script>
+
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<script>
+    var ctx = document.getElementById('pieChart').getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'pie',
+        data: {
+            labels: ['Barang Masuk', 'Barang Keluar'],
+            datasets: [{
+                label: 'Jumlah Barang',
+                data: [{{ $jumlahMasuk }}, {{ $jumlahKeluar }}],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.5)',
+                    'rgba(54, 162, 235, 0.5)',
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+        }
+    });
+</script>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,49 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Operator\DataBarangController;
+use App\Http\Controllers\Operator\DataSupplierController;
+use App\Http\Controllers\Operator\DataMasukController;
+use App\Http\Controllers\Operator\DataKeluarController;
+use App\Http\Controllers\Pimpinan\LaporanBarangController;
+use App\Http\Controllers\Pimpinan\LaporansupplierController;
+use App\Http\Controllers\Admin\DataPimpinanController;
+use App\Http\Controllers\Admin\DataOperatorController;
+use App\Http\Controllers\Admin\ProfileAdminController;
+// use App\Http\Controllers\Admin\ProfileOperatorController;
+// use App\Http\Controllers\Admin\ProfilePimpinanController;
 
-Route::get('/', function () {
-    return view('/layouts/app');
+
+// Route::get('/', function () {
+//     return view('/layouts/app');
+// });
+
+Route::redirect('/', '/login', 301);
+
+    Route::get('/dashboard', [HomeController::class, 'index'])->name('dashboard');
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard/admin', [HomeController::class, 'admin'])->name('dashboard.admin');
+    Route::post('/dashboard/admin', [HomeController::class, 'admin'])->name('dashboard.admin');
+    Route::get('/dashboard/operator', [HomeController::class, 'operator'])->name('dashboard.operator');
+    Route::post('/dashboard/operator', [HomeController::class, 'operator'])->name('dashboard.operator');
+    Route::get('/dashboard/pimpinan', [HomeController::class, 'pimpinan'])->name('dashboard.pimpinan');
+
+    //operator
+    Route::resource('/databarang', DataBarangController::class);
+    Route::resource('/datasupplier', DataSupplierController::class);
+    Route::resource('/barangmasuk', DataMasukController::class);
+    Route::resource('/barangkeluar', DataKeluarController::class);
+
+    //pimpinan
+    Route::resource('/laporanbarang', LaporanBarangController::class);
+    Route::resource('/laporansupplier', LaporansupplierController::class);
+    Route::get('/laporanbarang-cari', [LaporanBarangController::class, 'cari']);
+    Route::get('/laporansupplier-cari', [LaporansupplierController::class, 'cari']);
+    Route::get('/pdfsupplier', [LaporanBarangController::class, 'pdfsupplier']);
+    Route::get('/pdfbarang', [LaporanBarangController::class, 'pdfbarang']);
+
+    //admin
 });
